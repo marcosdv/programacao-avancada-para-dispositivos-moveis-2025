924eb4b2c0bf9c123648c1af65691c17272ebf2e
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2025-04-30 22:13:02 -0300

    Aula09 - 30-04-2025

---
+++ aula09_bancodados/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aula09_bancodados/ui/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Cadastros',
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
